8b54bd1173cfeba07577ee3642528be158f54ac8
dponfl <dponfl@gmail.com>
2023-02-24 20:18:04 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,21 @@ import { App } from './app.class.js';
 import { ChatGptCommunication } from './chatgpt/chatgpt.class.js';
 import { UseLogger } from './logger/logger.class.js';
 
-const hostname: string = process.env.PAPERTRAIL_HOSTNAME as string;
-const program: string = process.env.PAPERTRAIL_PROGRAM as string;
-const prompt: string = process.env.PROMPT as string;
+const bootstap = async () => {
 
-const logger = new UseLogger();
+	const hostname: string = process.env.PAPERTRAIL_HOSTNAME as string;
+	const program: string = process.env.PAPERTRAIL_PROGRAM as string;
+	const prompt: string = process.env.PROMPT as string;
 
-const app = new App(
-	logger,
-	new ChatGptCommunication(logger)
-);
+	const logger = new UseLogger();
 
-app.init();
-exit(0);
+	const app = new App(
+		logger,
+		new ChatGptCommunication(logger)
+	);
+
+	await app.init();
+
+};
+
+bootstap();
