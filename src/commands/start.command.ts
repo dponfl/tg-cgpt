116fb8f258f43f74bc89bf2545f56a6426a68c15
dponfl <dponfl@gmail.com>
2023-03-09 19:55:45 +0200

    debug logs

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -16,43 +16,55 @@ export class StartCommand extends MyBotCommand {
 	}
 
 	public async handle(): Promise<void> {
+
+		const errMsg = `Error at ${__filename}:handle => `;
+
 		this.bot.start(async (ctx) => {
-			await ctx.deleteMessage();
+			try {
 
-			/**
-			 * Create user record if none
-			 */
+				await ctx.deleteMessage();
 
-			if (ctx.session.botUserSession.userGuid) {
-				return;
+				/**
+				 * Create user record if none
+				 */
+
+				if (ctx.session.botUserSession.userGuid) {
+					return;
+				}
+
+				const guid = uuid.default.create().uuid;
+
+				const userRec: IUsersTable = {
+					guid,
+					firstname: ctx.from?.first_name || '',
+					surname: ctx.from?.last_name || '',
+					username: ctx.from?.username || '',
+					fromId: ctx.from?.id || '',
+					chatId: ctx.chat?.id || '',
+					region: 'RU',
+					country: 'RU',
+					messenger: Messenger.TELEGRAM,
+					clientUnreachable: false,
+					clientUnreachableDetails: '',
+					deleted: false,
+					banned: false,
+					lang: ctx.from?.language_code || 'RU'
+				};
+
+				const resRaw = await this.dbServices.usersDbService?.createUser(userRec);
+
+				if (resRaw?.status === DbResponseStatus.ERROR) {
+					throw new Error(`Error: cannot create user record`);
+				}
+
+				await ctx.scene.enter('startIntro');
+
+			} catch (error) {
+				if (error instanceof Error) {
+					this.logger.error(errMsg + error.message);
+				}
 			}
 
-			const guid = uuid.default.create().uuid;
-
-			const userRec: IUsersTable = {
-				guid,
-				firstname: ctx.from?.first_name || '',
-				surname: ctx.from?.last_name || '',
-				username: ctx.from?.username || '',
-				fromId: ctx.from?.id || '',
-				chatId: ctx.chat?.id || '',
-				region: 'RU',
-				country: 'RU',
-				messenger: Messenger.TELEGRAM,
-				clientUnreachable: false,
-				clientUnreachableDetails: '',
-				deleted: false,
-				banned: false,
-				lang: ctx.from?.language_code || 'RU'
-			};
-
-			const resRaw = await this.dbServices.usersDbService?.createUser(userRec);
-
-			if (resRaw?.status === DbResponseStatus.ERROR) {
-				throw new Error(`Error: cannot create user record`);
-			}
-
-			await ctx.scene.enter('startIntro');
 		});
 	}
 
