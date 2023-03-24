fcc257f595380454a6a933a1c27e4eb09a8d6d86
dponfl <dponfl@gmail.com>
2023-03-24 17:37:21 +0300

    timeout for textRequest

--- src/ai/open_ai/chat.service.ts
+++ src/ai/open_ai/chat.service.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { Configuration, OpenAIApi } from 'openai';
+import { resolve } from 'path';
 import { IConfigService } from '../../config/config.interface.js';
 import { AiTextResponse, AiResponseStatus, OpenAiChatFinishReason } from '../../controller/controller.interface.js';
 import { ILogger } from '../../logger/logger.interface.js';
@@ -88,136 +89,175 @@ export class OpenAiChatService implements IAIText {
 		return { ...params, ...paramsObj };
 	}
 
-	public async textRequest(userGuid: string, messages: IOpenAiChatMessage[]): Promise<AiTextResponse> {
+	// tslint:disable-next-line: promise-function-async
+	public textRequest(userGuid: string, messages: IOpenAiChatMessage[]): Promise<AiTextResponse> {
 
-		const methodName = 'textRequest';
+		// tslint:disable-next-line: no-shadowed-variable
+		return new Promise(async (resolve, reject) => {
+			const methodName = 'textRequest';
 
-		this.logger.info(`User: ${userGuid}, sending request to OpenAI (ChatGPT)`);
+			this.logger.info(`User: ${userGuid}, sending request to OpenAI (ChatGPT)`);
 
-		try {
+			try {
 
-			const majorParams: IChatRequestParams = {
-				model: OpenAiChatModels.GPT_3_5,
-				messages,
-			};
+				const majorParams: IChatRequestParams = {
+					model: OpenAiChatModels.GPT_3_5,
+					messages,
+				};
 
-			const requestParams: IChatRequestParams = await this.generateRequestParams(majorParams) as IChatRequestParams;
+				const requestParams: IChatRequestParams = await this.generateRequestParams(majorParams) as IChatRequestParams;
 
-			// TODO: delete
-			this.logger.warn(`User: ${userGuid}, requestParams:\n${JSON.stringify(requestParams)}`);
+				// TODO: delete
+				this.logger.warn(`User: ${userGuid}, requestParams:\n${JSON.stringify(requestParams)}`);
 
-			let requestCompleted: boolean = false;
+				const timeout = Number(this.configService.get('RESPONSE_TIMEOUT')) ?? Infinity;
 
-			const startTime = new Date().getTime();
+				let finishTimeOriginal;
 
-			// TODO: delete
-			// ===============================
-			this.utils.debugLogger(`Sending request to GPT`);
-			// ===============================
+				const timeOutId = setTimeout(() => {
 
-			// const response = await this.openai.createChatCompletion(requestParams);
+					finishTimeOriginal = new Date().getTime();
 
-			// TODO: delete
-			// ============================================================
-			const response = {
-				status: 200,
-				statusText: '200 OK',
-				data: {
-					choices: [
-						{
-							message: {
-								content: 'content'
+					const requestMetricsRecSuccess: IRequestsTable = {
+						userGuid,
+						system: Systems.CHATGPT,
+						subsystem: SubSustems.CHAT,
+						requestType: RequestTypes.STREAM,
+						requestStatus: RequestStatus.TIMEOUT,
+						duration: finishTimeOriginal - startTimeOriginal,
+					};
+
+					this.dbServices.requestsDbService?.create(requestMetricsRecSuccess)
+						.then(
+							// tslint:disable-next-line: no-any
+							(result: any) => {
+								if (result.status !== DbResponseStatus.SUCCESS) {
+									this.logger.error(`Requests metrics rec creation error. requestMetricsRec:\n${JSON.stringify(requestMetricsRecSuccess)}\nresult:\n${JSON.stringify(result)}`);
+								}
 							},
-							finish_reason: 'finish_reason'
+							// tslint:disable-next-line: no-any
+							(error: any) => {
+								this.logger.error(`Promise error: Requests metrics rec creation error. requestMetricsRec:\n${JSON.stringify(requestMetricsRecSuccess)}\nresult:\n${typeof error === 'string' ? error : JSON.stringify(error)}`);
+							}
+						);
+
+					reject(`Request timeout at ${methodName}`);
+				}, timeout);
+
+				const startTimeOriginal = new Date().getTime();
+
+				// TODO: delete
+				// ===============================
+				this.utils.debugLogger(`Sending request to GPT`);
+				// ===============================
+
+				// const response = await this.openai.createChatCompletion(requestParams);
+
+				// TODO: delete
+				// ============================================================
+				const response = {
+					status: 200,
+					statusText: '200 OK',
+					data: {
+						choices: [
+							{
+								message: {
+									content: 'content'
+								},
+								finish_reason: 'finish_reason'
+							}
+						],
+						usage: {
+							completion_tokens: 0,
+							prompt_tokens: 0,
+							total_tokens: 0
 						}
-					],
-					usage: {
-						completion_tokens: 0,
-						prompt_tokens: 0,
-						total_tokens: 0
 					}
+				};
+
+				await this.utils.sleep(5000);
+
+				this.utils.debugLogger(`Request to GPT completed`);
+
+				// ============================================================
+
+
+				finishTimeOriginal = new Date().getTime();
+
+				this.logger.info(`User: ${userGuid}, openai.createChatCompletion response:\nStatus: ${response.status} Status text: ${response.statusText} Data: ${JSON.stringify(response.data)}`);
+
+				const requestCount = this.utils.wordCounter(messages[messages.length - 1].content);
+
+				if (response.data.choices[0].message) {
+
+					const responseCount = this.utils.wordCounter(response.data.choices[0].message.content);
+
+					const requestMetricsRecSuccess: IRequestsTable = {
+						userGuid,
+						system: Systems.CHATGPT,
+						subsystem: SubSustems.CHAT,
+						requestType: RequestTypes.ORDINARY,
+						requestStatus: RequestStatus.SUCCESS,
+						duration: finishTimeOriginal - startTimeOriginal,
+						requestWords: requestCount.words,
+						responseWords: responseCount.words,
+						responseTokens: response.data.usage?.completion_tokens,
+						requestTokens: response.data.usage?.prompt_tokens,
+						totalTokens: response.data.usage?.total_tokens,
+					};
+
+					const createRequestMetricsRecRaw = await this.dbServices.requestsDbService?.create(requestMetricsRecSuccess);
+
+					if (createRequestMetricsRecRaw?.status !== DbResponseStatus.SUCCESS) {
+						this.logger.error(`Requests metrics rec creation error. requestMetricsRec:\n${JSON.stringify(requestMetricsRecSuccess)}\nresult:\n${JSON.stringify(createRequestMetricsRecRaw)}`);
+					}
+
+					resolve({
+						status: AiResponseStatus.SUCCESS,
+						finishReason: response.data.choices[0].finish_reason ?? null,
+						payload: response.data.choices[0].message.content
+					});
+
+					return;
+
+				} else {
+
+					const requestMetricsRecError: IRequestsTable = {
+						userGuid,
+						system: Systems.CHATGPT,
+						subsystem: SubSustems.CHAT,
+						requestType: RequestTypes.ORDINARY,
+						requestStatus: RequestStatus.ERROR,
+						duration: finishTimeOriginal - startTimeOriginal,
+						requestWords: requestCount.words,
+					};
+
+					const createRequestMetricsRecRaw = await this.dbServices.requestsDbService?.create(requestMetricsRecError);
+
+					if (createRequestMetricsRecRaw?.status !== DbResponseStatus.SUCCESS) {
+						this.logger.error(`Requests metrics rec creation error. requestMetricsRec:\n${JSON.stringify(requestMetricsRecError)}\nresult:\n${JSON.stringify(createRequestMetricsRecRaw)}`);
+					}
+
+					resolve({
+						status: AiResponseStatus.ERROR,
+						payload: ''
+					});
+
+					return;
+
 				}
-			};
 
-			await this.utils.sleep(5000);
-
-			this.utils.debugLogger(`Request to GPT completed`);
-
-			// ============================================================
-
-
-			const finishTime = new Date().getTime();
-
-			this.logger.info(`User: ${userGuid}, openai.createChatCompletion response:\nStatus: ${response.status} Status text: ${response.statusText} Data: ${JSON.stringify(response.data)}`);
-
-			requestCompleted = true;
-
-			const requestCount = this.utils.wordCounter(messages[messages.length - 1].content);
-
-			if (response.data.choices[0].message) {
-
-				const responseCount = this.utils.wordCounter(response.data.choices[0].message.content);
-
-				const requestMetricsRecSuccess: IRequestsTable = {
-					userGuid,
-					system: Systems.CHATGPT,
-					subsystem: SubSustems.CHAT,
-					requestType: RequestTypes.ORDINARY,
-					requestStatus: RequestStatus.SUCCESS,
-					duration: finishTime - startTime,
-					requestWords: requestCount.words,
-					responseWords: responseCount.words,
-					responseTokens: response.data.usage?.completion_tokens,
-					requestTokens: response.data.usage?.prompt_tokens,
-					totalTokens: response.data.usage?.total_tokens,
-				};
-
-				const createRequestMetricsRecRaw = await this.dbServices.requestsDbService?.create(requestMetricsRecSuccess);
-
-				if (createRequestMetricsRecRaw?.status !== DbResponseStatus.SUCCESS) {
-					this.logger.error(`Requests metrics rec creation error. requestMetricsRec:\n${JSON.stringify(requestMetricsRecSuccess)}\nresult:\n${JSON.stringify(createRequestMetricsRecRaw)}`);
-				}
-
-				return {
-					status: AiResponseStatus.SUCCESS,
-					finishReason: response.data.choices[0].finish_reason ?? null,
-					payload: response.data.choices[0].message.content
-				};
-			} else {
-
-				const requestMetricsRecError: IRequestsTable = {
-					userGuid,
-					system: Systems.CHATGPT,
-					subsystem: SubSustems.CHAT,
-					requestType: RequestTypes.ORDINARY,
-					requestStatus: RequestStatus.ERROR,
-					duration: finishTime - startTime,
-					requestWords: requestCount.words,
-				};
-
-				const createRequestMetricsRecRaw = await this.dbServices.requestsDbService?.create(requestMetricsRecError);
-
-				if (createRequestMetricsRecRaw?.status !== DbResponseStatus.SUCCESS) {
-					this.logger.error(`Requests metrics rec creation error. requestMetricsRec:\n${JSON.stringify(requestMetricsRecError)}\nresult:\n${JSON.stringify(createRequestMetricsRecRaw)}`);
-				}
-
-				return {
-					status: AiResponseStatus.ERROR,
-					payload: ''
-				};
+			} catch (error) {
+				const errText = this.utils.errorLog(this, error, methodName);
+				reject(errText);
 			}
-
-		} catch (error) {
-			return {
-				status: AiResponseStatus.ERROR,
-				payload: this.utils.errorLog(this, error, methodName)
-			};
-		}
+		});
 	}
 
 	// tslint:disable-next-line: promise-function-async
 	public textStreamRequest(userGuid: string, messages: IOpenAiChatMessage[]): Promise<AiTextResponse> {
 
+		// tslint:disable-next-line: no-shadowed-variable
 		return new Promise(async (resolve, reject) => {
 			const methodName = 'textStreamRequest';
 
@@ -354,7 +394,7 @@ export class OpenAiChatService implements IAIText {
 
 			} catch (error) {
 				const errText = this.utils.errorLog(this, error, methodName);
-				throw new Error(errText);
+				reject(errText);
 			}
 		});
 
