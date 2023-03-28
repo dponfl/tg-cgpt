9d259434afd32b9ba64a223a1e86df8ddea0cf4d
dponfl <dponfl@gmail.com>
2023-03-28 22:53:07 +0300

    check puppeteer func

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,8 @@ import { ServiceUsageStorageService } from './storage/service.class.js';
 import { RequestStorageService } from './storage/request.class.js';
 import { MjService } from './ai/mj/mj.service.js';
 
+import puppeteer from 'puppeteer-core';
+
 
 const bootstap = async () => {
 
@@ -283,15 +285,45 @@ const bootstap = async () => {
 
 	})();
 
-	mjService.imgRequest('userGuid', 'test prompt')
-		.then(
-			async (result) => {
-				logger.info(`mjService.imgRequest: resolved`);
-			},
-			async (error) => {
-				logger.error(`mjService.imgRequest: rejected`);
-			}
+	// mjService.imgRequest('userGuid', 'test prompt')
+	// 	.then(
+	// 		async (result) => {
+	// 			logger.info(`mjService.imgRequest: resolved`);
+	// 		},
+	// 		async (error) => {
+	// 			logger.error(`mjService.imgRequest: rejected`);
+	// 		}
+	// 	);
+
+
+	(async () => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+
+		await page.goto('https://developer.chrome.com/');
+
+		// Set screen size
+		await page.setViewport({ width: 1080, height: 1024 });
+
+		// Type into search box
+		await page.type('.search-box__input', 'automate beyond recorder');
+
+		// Wait and click on first result
+		const searchResultSelector = '.search-box__link';
+		await page.waitForSelector(searchResultSelector);
+		await page.click(searchResultSelector);
+
+		// Locate the full title with a unique string
+		const textSelector = await page.waitForSelector(
+			'text/Customize and automate'
 		);
+		const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+		// Print the full title
+		logger.info('The title of this blog post is "%s".', fullTitle);
+
+		await browser.close();
+	})();
 
 	exit;
 
