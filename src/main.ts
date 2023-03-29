c8266d4d46c054b79c90810741a1d2c97db2fe31
dponfl <dponfl@gmail.com>
2023-03-29 10:51:54 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ import { ServiceUsageStorageService } from './storage/service.class.js';
 import { RequestStorageService } from './storage/request.class.js';
 import { MjService } from './ai/mj/mj.service.js';
 
-import puppeteer from 'puppeteer-core';
+import puppeteer from 'puppeteer';
 import { join } from 'path';
 import * as url from 'url';
 
